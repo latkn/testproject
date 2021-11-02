d16df1a612ff5e5c19aeb437b2509ff5f8bad0c5
Mikhail Latkin <latkin.misha@ya.ru>
2021-11-02 10:05:59 +0500

    Рефакторинг кода, функции теперь методы объекта personalMovieDB

--- script.js
+++ script.js
@@ -13,22 +13,17 @@ let personalMovieDB = {
 // start();
 // askUser(); 
 // getAnswers(); 
-writeYourGenres(); 
-//
 
 
-function start() {
+
+ personalMovieDB.start = function() {
     numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
     while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
         numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?');
     }
 }
 
-
-
-
-
-function askUser() {
+personalMovieDB.askUser = function() {
     for (let i = 0; i < 2; i++) {
         let firstQuestion = prompt(' Один из последних просмотренных фильмов? ');
         let secondQuestion = prompt(' На сколько оцените его? ');
@@ -42,7 +37,7 @@ function askUser() {
 
 
 
-function getAnswers() {
+personalMovieDB.getAnswers = function() {
     if (personalMovieDB.count < 10) alert ('Просмотрено довольно мало фильмов');
     if (personalMovieDB.count > 10 && personalMovieDB.count < 30) alert('Вы классический зритель');
     if (personalMovieDB.count > 30) {
@@ -53,23 +48,37 @@ function getAnswers() {
 
 }
 
+personalMovieDB.showMyDB =  function() {
+        console.log (personalMovieDB.privat);
 
+}
 
-function showMyDB () {
+personalMovieDB.toggleVisibleMyDB = function() {
     if (personalMovieDB.privat == false) {
-        console.log (personalMovieDB);
-    }
+        personalMovieDB.privat = true; 
+    } else personalMovieDB.privat = false;
 }
-
-
  
-function writeYourGenres () {
+personalMovieDB.writeYourGenres = function() {
+    let writeData;     
     for (let i = 1; i<4; i++) {
-        personalMovieDB.genres.push(prompt (`Ваш любимый жанр под номером ${i}`, ""));
-        console.log ('записано '+ personalMovieDB.genres);
-    }
+            writeData = (prompt (`Ваш любимый жанр под номером ${i}`, ""));
+            if (writeData == "" || writeData == null) {
+                 i--;
+                 continue; 
+            }
+            personalMovieDB.genres.push(writeData);
+            console.log ('записано '+ personalMovieDB.genres);
+        }
+    personalMovieDB.genres.forEach(element => console.log(`любимый жанр №${(personalMovieDB.genres.indexOf(element)+1)} - это ${element}`));
 }
 
-console.log(personalMovieDB.movies);
-console.log(personalMovieDB);
-console.log(personalMovieDB.genres);
+//console.log(`Это сам объект: ${Object.keys(personalMovieDB)}`);
+//console.log(personalMovieDB.movies);
+//console.log(personalMovieDB.genres);
+
+personalMovieDB.writeYourGenres(); 
+personalMovieDB.showMyDB();
+personalMovieDB.toggleVisibleMyDB();
+personalMovieDB.showMyDB();
+
